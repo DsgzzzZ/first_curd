fc9ca6d174a985754047e9d3ca9e2fb2a93dc35c
DDZ <2994127020@qq.com>
2020-03-31 14:28:46 +0800

    自定义一个IllegalNameException异常.

---
+++ demo/src/main/java/com/ddz/demo/Exception/IllegalNameException.java
@@ -0,0 +1,11 @@
+package com.ddz.demo.Exception;
+
+public class IllegalNameException extends Exception {
+    public IllegalNameException(){
+
+    }
+
+    public IllegalNameException(String str){
+        super(str);
+    }
+}
