8e8a94c624c9ee2018ce23795fcf734a799e85cd
DDZ <2994127020@qq.com>
2020-04-15 16:06:41 +0800

    4.15集成Druid和Redis

--- demo/src/main/java/com/ddz/demo/DemoApplication.java
+++ demo/src/main/java/com/ddz/demo/DemoApplication.java
@@ -3,6 +3,7 @@ package com.ddz.demo;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
@@ -11,6 +12,7 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
 * */
 @SpringBootApplication
 @EnableScheduling
+@EnableCaching //启动缓存 redis
 @EnableTransactionManagement
 @MapperScan("com.ddz.demo.dao")
 public class DemoApplication {
